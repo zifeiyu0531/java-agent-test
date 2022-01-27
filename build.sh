0fb05808ee2d06f1df40289db0b208b779428f76
zifeiyu0531 <32443516+zifeiyu0531@users.noreply.github.com>
2022-01-27 12:38:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # download pinpoint and unzip
-wget -O MseAgent.tar.gz https://github.com/pinpoint-apm/pinpoint/releases/download/v2.3.3/pinpoint-agent-2.3.3.tar.gz
-tar -zxvf MseAgent.tar.gz
+wget -O PolarisAgent.tar.gz https://github.com/pinpoint-apm/pinpoint/releases/download/v2.3.3/pinpoint-agent-2.3.3.tar.gz
+tar -zxvf PolarisAgent.tar.gz
 
 # modify content
-mv pinpoint-agent-2.3.3 MseAgent
-rm -rf MseAgent/plugin/*
+mv pinpoint-agent-2.3.3 PolarisAgent
+rm -rf PolarisAgent/plugin/*
 
 # build with maven
 mvn -B package --file pom.xml
 
 # add plugin
-mv polaris-agent-adapter/pinpoint/pinpoint-dubbo2-plugin/output/pinpoint-dubbo2-plugin-1.0.0-SNAPSHOT-jar-with-dependencies.jar MseAgent/plugin/
-mv polaris-agent-adapter/pinpoint/pinpoint-spring-cloud-2021-plugin/output/pinpoint-spring-cloud-2021-plugin-1.0.0-SNAPSHOT-jar-with-dependencies.jar MseAgent/plugin/
+mv -r target/* PolarisAgent/plugin/
 
 # zip
-zip -r MseAgent.zip MseAgent
-
-
-
+zip -r PolarisAgent.zip PolarisAgent
